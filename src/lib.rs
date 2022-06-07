4a5b0773765522a0a0c677d67ee76748c4c46947
timothyAgevi <timothyagevi@gmail.com>
2022-06-07 21:22:47 +0300

    add hello_World function

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,3 @@
-/*
- * This is an example of a Rust smart contract with two simple, symmetric functions:
- *
- * 1. set_greeting: accepts a greeting, such as "howdy", and records it for the user (account_id)
- *    who sent the request
- * 2. get_greeting: accepts an account_id and returns the greeting saved for it, defaulting to
- *    "Hello"
- *
- * Learn more about writing NEAR smart contracts with Rust:
- * https://github.com/near/near-sdk-rs
- *
- */
-
 // To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen, setup_alloc};
@@ -36,7 +23,9 @@ impl Default for Welcome {
 
 #[near_bindgen]
 impl Welcome {
-   
+   pub fn hello_world( $self)->String{
+       "Hello World from Rust Smart Contract".to_String()
+   }
 }
 
 
